2fecd874d19fd379344b6ee81d00d0d31a276ad4
Mohamed-Abdallah01 <mohammmed.abdullah114@gmail.com>
2024-10-06 08:46:31 +0300

    see the added features

--- Advanced_Machine_learning_Amit_course/Matrail/PythonForML/tasks/task1.py
+++ Advanced_Machine_learning_Amit_course/Matrail/PythonForML/tasks/task1.py
@@ -1,2 +1,14 @@
 new = 10 
 print(type(new))
+
+print('kello world')
+#do not work cause they are comments
+
+# print('kello world')
+# print('kello world')
+
+print('kello world')
+print('kello world')
+print('kello world')
+print('kello world')
+
