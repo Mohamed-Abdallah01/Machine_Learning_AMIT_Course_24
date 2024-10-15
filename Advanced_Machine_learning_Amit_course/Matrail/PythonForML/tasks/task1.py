2bb35a29a6105782fe90f5cc92e5c80fca1b676e
Mohamed-Abdallah01 <mohammmed.abdullah114@gmail.com>
2024-10-15 03:12:46 +0300

    We create a new repo now

--- Advanced_Machine_learning_Amit_course/Matrail/PythonForML/tasks/task1.py
+++ Advanced_Machine_learning_Amit_course/Matrail/PythonForML/tasks/task1.py
@@ -1,14 +1,36 @@
-new = 10 
-print(type(new))
+#Task-1
 
-print('kello world')
-#do not work cause they are comments
+Email = 'Amit-ML@gmail.com'
 
-# print('kello world')
-# print('kello world')
+def validation_mail(Email):
 
-print('kello world')
-print('kello world')
-print('kello world')
-print('kello world')
-print('age')
+    if Email.count('َ@') == 1:
+        return('valid Mail')
+    
+    username , domain = Email.split('@')
+    
+    if '.' not in domain:
+        return('Invalid Mail')
+    
+    return "Vaild Mail"
+
+result = validation_mail(Email)
+print(result)
+
+#print user name
+print(Email[0:7])
+#Extract Domain
+print(Email[-3:])
+
+
+def domain_check(Email):
+    if Email[-4:] == '.com':
+        return 'Commercial Email'
+    elif Email[-4:] == '.edu':
+        return 'Eductional Email'
+    
+    return 'Other Domain Name '
+
+result2 = domain_check(Email)
+print(result2)
+
